d5f9f74805aee5a1476178033f86fd99785d54d9
mrun@l <mrunalmore22@gmail.com>
2024-08-30 13:33:46 +0530

    added crud

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,11 +6,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable, HasApiTokens;
+    use HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
@@ -41,6 +40,7 @@ class User extends Authenticatable
     protected function casts(): array
     {
         return [
+            'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
     }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 use App\Http\Controllers\CalculatorController;
 
 Route::get('/', function () {
@@ -22,5 +23,17 @@ Route::get('/contact', function () {
 
 //Route::post('/calculate', [CalculatorController::class, 'Calculate'])->name('calculate');
 
+
+// routes for calculator
 Route::get('/calculator', [CalculatorController::class, 'index']);
 Route::post('/calculate', [CalculatorController::class, 'calculate']);
+
+// routes for crud
+Route::controller(ProductController::class)->group(function(){
+    Route::get('/products','index')->name('products.index');
+    Route::get('/products/create','create')->name('products.create');
+    Route::post('/products','store')->name('products.store');
+    Route::get('/products/{product}/edit','edit')->name('products.edit');
+    Route::put('/products/{product}','update')->name('products.update');
+    Route::delete('/products/{product}','destroy')->name('products.destroy');    
+});
